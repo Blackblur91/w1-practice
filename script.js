bd3dc80d738cfdb32c7b084029b330869c43479d
Blackblur91 <feketebalazs04@gmail.com>
2023-09-29 10:45:51 +0200

    new branch - friday

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-function logger(param) {
-  //let param = "teknősbéka"
-  console.log(param)
-}
+console.log("Hello World")
 
-logger("kismacska") // FÜÜGVÉNY MEGHÍVÁSA -> ILYENKOR (ÉS CSAK ILYENKOR) FUT LE
-
-
-function sumOfTwoNummbers(number1, number2) {
-  //console.log(number1 + number2)
-  let sum = number1 + number2  
-  return sum
-}
-
-/* let result = sumOfTwoNummbers(2,4)
-  logger(result) */
-
-  logger(sumOfTwoNummbers(5, 10))
-
+for (let i = i <=5; i++
+  )
--- scripts/script2.js
+++
@@ -1,7 +0,0 @@
-let variable = "kis"
-let variable2 = "macska"
-
-console.log("100"+ "2");
-console.log(100+2);
-console.log("variable")
-console.log(variable)
